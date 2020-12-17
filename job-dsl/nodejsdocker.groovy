3177e85630d039cd7ac6d76c771c53a714597d24
ranga17A <75625835+ranga17A@users.noreply.github.com>
2020-12-17 09:48:27 -0500

    Add files via upload

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('anusha17/docker-nodejs-demo')
+            repositoryName('wardviaene/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
